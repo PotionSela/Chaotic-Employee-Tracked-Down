109c634a01509e328dfcc93fc5d40dd3dd38225d
Gisela <giselamata27@gmail.com>
2023-12-23 15:06:40 -0500

    Forgot to add department id's

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
-INSERT INTO department ( department_name )
-VALUES ("Legal"),
-       ("Human Resources"),
-       ("Customer Support"),
-       ("Sales"),
-       ("Operations"),
-       ("Marketing"),
-       ("Finance");
+INSERT INTO department ( id, department_name )
+VALUES (1, "Legal"),
+       (2, "Human Resources"),
+       (3, "Customer Support"),
+       (4, "Sales"),
+       (5, "Operations"),
+       (6, "Marketing"),
+       (7, "Finance");
 
 INSERT INTO role ( id, title, salary, department_id )
-VALUES (1, ""),
-       (2, ""),
-       (1, ""),
-       (3, ""),
-       (5, ""),
-       (1, ""),
-       (5, "");
+VALUES (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       ();
