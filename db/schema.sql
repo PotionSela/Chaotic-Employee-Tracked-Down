3b48a19a937ad2da0ac6714c9ecc79e7539a12be
Gisela <giselamata27@gmail.com>
2023-12-23 14:44:22 -0500

    Created table for role, not finished

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,10 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+)
