56e648728279fccf2a9f4edd056dc84cf85e2f0d
Gisela <giselamata27@gmail.com>
2023-12-23 14:57:56 -0500

    Created department names

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (department_name)
+VALUES ("Legal"),
+       ("Human Resources"),
+       ("Customer Support"),
+       ("Sales"),
+       ("Operations"),
+       ("Marketing"),
+       ("Finance");
+
