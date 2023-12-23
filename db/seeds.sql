d5ba6416097db8ff6bd9c922233aa075877467a6
Gisela <giselamata27@gmail.com>
2023-12-23 15:04:03 -0500

    Added outline of instert into role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (department_name)
+INSERT INTO department ( department_name )
 VALUES ("Legal"),
        ("Human Resources"),
        ("Customer Support"),
@@ -7,3 +7,11 @@ VALUES ("Legal"),
        ("Marketing"),
        ("Finance");
 
+INSERT INTO role ( id, title, salary, department_id )
+VALUES (1, ""),
+       (2, ""),
+       (1, ""),
+       (3, ""),
+       (5, ""),
+       (1, ""),
+       (5, "");
