29fc08411e26770ccae1ecd663b07520048c5bf0
Gisela <giselamata27@gmail.com>
2023-12-26 14:14:55 -0500

    Created Model folder and made files for the schema

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express Middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connecting to database
-const db = mysql.createConnection (
+// Creating a connection to database
+const db = mysql.createConnection(
     {
         host: 'localhost',
         // MySQL username
         user: 'root',
         // MySQL password
         password: '',
-        database: 'tracker_db'
+        database: 'tracker_db',
     },
     console.log(`Connected to the tracker_db database.`)
 );
