df52d787b7d04f6d33e88c726c45767f1ce75b90
Gisela <giselamata27@gmail.com>
2023-12-26 14:36:42 -0500

    Deleted unneeded files and importing inquirer

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mysql = require('mysql2');
 // Importing mysql
 
+const inquirer = require("inquirer");
+// Importing inquirer
+
 // Port import
 const PORT = process.env.PORT || 3001;
 const app = express();
