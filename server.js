ad2dba50181702fcc55a5b78e03ed74dbdd5e30d
Gisela <giselamata27@gmail.com>
2023-12-26 12:23:13 -0500

    Added connection to database

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ const app = express();
 // Express Middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connecting to database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'tracker_db'
+    },
+    console.log(`Connected to the tracker_db database.`)
+)
